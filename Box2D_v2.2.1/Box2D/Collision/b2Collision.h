7d8ac761812753245119a0cd01675a20389f98bc
jarbot <jarbot@gmail.com>
2014-06-18 11:08:11 -0700

    reverting changes to see if performance hit is corrected

--- Box2D_v2.2.1/Box2D/Collision/b2Collision.h
+++ Box2D_v2.2.1/Box2D/Collision/b2Collision.h
@@ -39,8 +39,6 @@ const uint8 b2_nullFeature = UCHAR_MAX;
 /// This must be 4 bytes or less.
 struct b2ContactFeature
 {
-	b2ContactFeature() = default;
-
 	enum Type
 	{
 		e_vertex = 0,
@@ -72,8 +70,6 @@ union b2ContactID
 /// provide reliable contact forces, especially for high speed collisions.
 struct b2ManifoldPoint
 {
-	b2ManifoldPoint() {}
-
 	b2Vec2 localPoint;		///< usage depends on manifold type
 	float32 normalImpulse;	///< the non-penetration impulse
 	float32 tangentImpulse;	///< the friction impulse
@@ -98,8 +94,6 @@ struct b2ManifoldPoint
 /// This structure is stored across time steps, so we keep it small.
 struct b2Manifold
 {
-	b2Manifold() {}
-
 	enum Type
 	{
 		e_circles,
@@ -110,15 +104,13 @@ struct b2Manifold
 	b2ManifoldPoint points[b2_maxManifoldPoints];	///< the points of contact
 	b2Vec2 localNormal;								///< not use for Type::e_points
 	b2Vec2 localPoint;								///< usage depends on manifold type
-	b2Manifold::Type type;
+	Type type;
 	int32 pointCount;								///< the number of manifold points
 };
 
 /// This is used to compute the current state of a contact manifold.
 struct b2WorldManifold
 {
-	b2WorldManifold() {}
-
 	/// Evaluate the manifold with supplied transforms. This assumes
 	/// modest motion from the original state. This does not change the
 	/// point count, impulses, etc. The radii must come from the shapes
@@ -148,29 +140,21 @@ void b2GetPointStates(b2PointState state1[b2_maxManifoldPoints], b2PointState st
 /// Used for computing contact manifolds.
 struct b2ClipVertex
 {
-	b2ClipVertex() {}
-
 	b2Vec2 v;
 	b2ContactID id;
 };
 
 /// Ray-cast input data. The ray extends from p1 to p1 + maxFraction * (p2 - p1).
-// emscripten - b2RayCastInput: add constructor
 struct b2RayCastInput
 {
-	b2RayCastInput() {}
-	b2Vec2 p1;
-	b2Vec2 p2;
+	b2Vec2 p1, p2;
 	float32 maxFraction;
 };
 
 /// Ray-cast output data. The ray hits at p1 + fraction * (p2 - p1), where p1 and p2
 /// come from b2RayCastInput.
-// emscripten - b2RayCastOutput: add constructor
 struct b2RayCastOutput
 {
-   	b2RayCastOutput() {}
-
 	b2Vec2 normal;
 	float32 fraction;
 };
